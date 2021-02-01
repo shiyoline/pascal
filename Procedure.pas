9bfcae9e5525b9e3ba65c1190fecf85861fa789d
Thang Cao <shiyoline@gmail.com>
2021-02-01 09:24:14 -0800

    Add a finding minimum procedure

---
+++ Procedure.pas
@@ -0,0 +1,23 @@
+program Procedure;
+var
+   a, b, c,  min: integer;
+procedure findMin(x, y, z: integer; var m: integer); (*variable m is pass by reference *)
+(* Finds the minimum of the 3 values *)
+
+begin
+   if x < y then
+      m:= x
+   else
+      m:= y;
+   
+   if z < m then
+      m:= z;
+end; { end of procedure findMin }  
+
+begin
+   writeln(' Enter three numbers: ');
+   readln( a, b, c);
+   findMin(a, b, c, min); (* Procedure call *)
+   
+   writeln(' Minimum: ', min);
+end.
